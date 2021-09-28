06a3e25b6ebdffe1a3f16cb5424e72150594f4c9
AdelinoJnr <adelinojunior96@gmail.com>
2021-09-28 18:34:35 -0300

    feat: heroku

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const PORT = process.env.PORT || 3000;
+
+const upsideDown = process.env.UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = /true/i.test(upsideDown);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
