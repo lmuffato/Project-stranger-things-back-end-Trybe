3223b033743a3750f5aec0bc64a45738a67d6194
Jodiel Briesemeister <jodielll29@gmail.com>
2021-09-29 17:06:35 -0300

    tt

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ app.get('/', (req, res) => {
 const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log(`Escutando na porta: ${port}`);
+  console.log(`Escutando na porta ${port}`);
 });
