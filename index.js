8c4394a48234b11bba2c78dc8d0c3e6d533484e4
Orlando Flores <orlando_flores@id.uff.br>
2021-09-29 16:55:28 -0300

    fix: variaveis de ambiente

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
