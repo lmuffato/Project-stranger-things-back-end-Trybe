e57ea1c24efb8fed7cb06a472efb779e847fa635
maricegalla <maricegalla@gmail.com>
2021-09-29 15:41:45 -0300

    Requirement 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -16,17 +17,19 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const { UPSIDEDOWN_MODE } = process.env;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    UPSIDEDOWN_MODE,
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
