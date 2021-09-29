b07f8c48b8c8c8a91e8255d90f9d3cb60b956ccb
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-09-29 19:03:46 -0300

    Feat: (req-1) Cria as variáveis PORT e UPSIDEDOWN_MODE de ambiente no arquivo .env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express'); // BackEnd
 const cors = require('cors');
 
+const { PORT, UPSIDEDOWN_MODE } = process.env;
+
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -16,17 +18,17 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+// const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    UPSIDEDOWN_MODE,
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
