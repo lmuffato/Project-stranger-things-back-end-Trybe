7784898cfd8dbcb16cfacbcde0ea59e16f52f063
Victor Canto <iamvictorcanto@gmail.com>
2021-09-29 16:53:15 -0300

    feat: create environment variables

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
+require('dotenv').config();
+
+const { PORT, UPSIDEDOWN_MODE } = process.env;
+
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
@@ -16,7 +20,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +31,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
