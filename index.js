4031dbffc1166ebdbf3818a2711509625cab5e57
Gabriel <g.pereira.a.m@gmail.com>
2021-09-29 16:24:03 -0300

    feat(Req 1) - :sparkle: Criando as variaveis de ambiente

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -16,7 +18,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = (process.env.UPSIDEDOWN_MODE === 'true');
+const port = parseInt(process.env.PORT, 10);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Escutando na porta 3000');
 });
