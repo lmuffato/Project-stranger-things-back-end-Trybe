304e2361d807d5f9ccb466ff8dd429471a789f4c
Elisa <elisafranca.carvalho@gmail.com>
2021-09-29 15:51:46 -0300

    feat: implements 1st requirement

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ const strangerThingsService = new StrangerThingsService(
 );
 
 app.use(cors());
+const { PORT, UPSIDEDOWN_MODE } = process.env;
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +28,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
