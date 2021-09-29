2f493865e493283d2cf06dab4090f85f771f5c2a
Alexandre Oliveira <alexandremolive@gmail.com>
2021-09-29 18:32:01 -0300

    req 5

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -14,9 +15,10 @@ const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
 
-app.use(cors());
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
+const port = process.env.PORT;
 
-const hereIsTheUpsideDown = true;
+app.use(cors());
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +29,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(port, () => {
+  console.log(`Escutando na porta ${port}`);
 });
