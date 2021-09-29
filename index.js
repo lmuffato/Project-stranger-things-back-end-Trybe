02c9e2da5720698531c56d0991a621640f9cb354
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-09-29 17:05:12 -0300

    ProcFile added and package.json modified to receive pm2

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
