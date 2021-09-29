6c405e1aa8f90844109b75e88ab8c43fdb271663
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-09-29 17:53:36 -0300

    fix:lint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const { PORT } = process.env;
-const intPort =parseInt(PORT,10)
+const intPort = parseInt(PORT, 10);
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
