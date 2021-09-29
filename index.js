3369302360ab775ee4f01f2f667bdcda559f1d3e
mallmann02 <leomallmann020302@gmail.com>
2021-09-29 17:58:41 -0300

    refactor: create PORT const

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT, () => {
-  console.log('Escutando na porta 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
