a1e14143f5a60b99a5873eb217ef6619d64bb6a3
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-09-28 21:49:14 -0400

    feat: Backend - Req 1 - Verifica as variáveis de ambiente

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
