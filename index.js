5d98c77b5fa40bb788f3d2789585c247c98f2406
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-09-29 17:28:38 -0400

    alterando index

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -10,13 +12,14 @@ const app = express();
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
 );
+
 const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE !== 'false';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
