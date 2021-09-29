26da451ca4d846ff5a21123563effd9da4e79075
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-09-29 18:29:37 -0300

    fix:port/upsidedown

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const { PORT } = process.env;
-const intPort = parseInt(PORT, 10);
+const PORT = process.env.PORT || 3000;
+// const intPort = parseInt(PORT, 10);
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -19,18 +19,18 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
-const parseUpsideDown = JSON.parse(hereIsTheUpsideDown);
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || false;
+// const parseUpsideDown = JSON.parse(hereIsTheUpsideDown);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    parseUpsideDown,
+    hereIsTheUpsideDown,
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(intPort, () => {
+app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
 });
