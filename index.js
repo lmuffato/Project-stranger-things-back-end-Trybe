34aeb3e4198fffdb24c63a1b055168904853430b
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-09-29 16:56:28 -0300

    backend strangerThings

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv/config');
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
+
+const PORT = process.env.PORT || 3000;
+const { UPSIDEDOWN_MODE } = process.env;
+
+const convertVarEnvToBool = (varEnv) => {
+  if (varEnv === 'true') return true;
+  return false;
+};
 
 const app = express();
 
@@ -16,7 +25,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = convertVarEnvToBool(UPSIDEDOWN_MODE);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +36,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
