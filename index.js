01ff9b32d94f4502e1f75b86b1c63e45e5e05c8f
giovanni-maldonado <giovannitadeu08@gmail.com>
2021-09-29 16:19:07 -0300

    feat: requisitos 1 a 5

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -27,6 +28,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
