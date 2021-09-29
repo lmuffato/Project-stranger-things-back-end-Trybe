2ddb1caab0a330ae2ef56dc0542864cef4b3ab10
Luciano Amâncio <madruga665@hotmail.com>
2021-09-29 20:58:28 -0300

    make it better

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 const { UPSIDEDOWN_MODE } = process.env;
 
 const strangerThingsRepository = new StrangerThingsRepository(
