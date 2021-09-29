fb81369014bf79c8c5b541b24650a7ccd5475349
maricegalla <maricegalla@gmail.com>
2021-09-29 17:44:57 -0300

    Heroku Deploy

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
