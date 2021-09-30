fa2fecb33cfef981112108c5644df813b17a8f48
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-29 21:27:14 -0300

    fix: deploy app upside-down

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 // const hereIsTheUpsideDown = true;
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || true;
-// const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true'; 
+// const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-const PORT = process.env.PORT || 3000;
+const { PORT } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
