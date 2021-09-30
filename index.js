b234119f6a1b2782bfb9a870877ede7382ddefc9
rafael <rafittu@outlook.com>
2021-09-30 20:48:47 -0300

    lendo porta via envar

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -17,17 +17,17 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const UPSIDEDOWN_MODE = true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    UPSIDEDOWN_MODE,
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(port, () => {
-  console.log(`O pai ta on na ${port}`);
+app.listen(PORT, () => {
+  console.log(`O pai ta on na ${PORT}`);
 });
