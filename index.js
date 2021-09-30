83e0bf3cd96fce17248b48cf1835757aaecc8b68
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-09-30 17:15:54 -0400

    feat: :sparkles: req 1

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
+const { PORT } = process.env;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
-//
+
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
@@ -27,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
