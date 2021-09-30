851436cdd0d76d31f318989a5d9ef30b6c0dd40e
AndreBarroso <andre_barroso17@hotmail.com>
2021-09-29 22:34:42 -0300

    fix: hawkins

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
