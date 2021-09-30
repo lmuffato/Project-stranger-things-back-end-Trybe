78838f862c655f844803483cc95b56cb1ecac912
Elisa <elisafranca.carvalho@gmail.com>
2021-09-30 09:26:41 -0300

    feat: add user variable - fix: scripts and upsideDown value

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 const { PORT, UPSIDEDOWN_MODE } = process.env;
 
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
