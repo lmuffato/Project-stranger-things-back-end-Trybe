310a4df2e054cbf0ed0fdb2a484b292ed0dd6a27
Luciano Amâncio <madruga665@hotmail.com>
2021-09-29 22:18:08 -0300

    make it better

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(PORT || 3000, () => {
+app.listen(PORT, () => {
   console.log(`Escutando na porta app ${PORT}`);
 });
