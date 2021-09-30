1bf82c36c47ab1c2a2eaa868933a2da1f65fbd37
MaxRudim <max.hermsdorff@gmail.com>
2021-09-29 22:32:06 -0300

    Req. 01 - PORT e UPSIDEDOWN_MODE no .env

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 
+require('dotenv').config();
+
+const { PORT, UPSIDEDOWN_MODE } = process.env;
+
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -16,18 +20,18 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+// const hereIsTheUpsideDown = true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    // hereIsTheUpsideDown,
+    UPSIDEDOWN_MODE,
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
-//
