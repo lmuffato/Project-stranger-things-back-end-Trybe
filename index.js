a487ac9fd86a4023b0e0d5ed48d71b69b8403e98
Eduardo Seije <eduardoseije@gmail.com>
2021-09-30 15:46:50 -0300

    ajuste variável

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 const { PORT } = process.env;
 
 app.get('/', (req, res) => {
