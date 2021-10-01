1a5b5a1f19b870b6704e6df7c3feaa46f498597b
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-10-01 19:13:23 -0300

    feat: atualizando as portas da aplicação

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv');
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -27,6 +28,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Escutando na porta ${port}`);
 });
