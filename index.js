0efc5fd12f9f27ca10f7bbca1fd57be67210df09
Marcelo Maurício <mmauricio.design@gmail.com>
2021-10-01 10:37:56 -0300

    fix: the checker about mode

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 const PORT = process.env.PORT || 3000;
 
