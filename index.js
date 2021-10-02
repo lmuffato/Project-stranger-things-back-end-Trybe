977b78abafefbd6dd0c02289e7ffd582ada3cf7c
w-cypriano <profwellington17@gmail.com>
2021-10-01 21:35:04 -0300

    subindo primeiro app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+
 require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
@@ -7,7 +8,6 @@ const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
-const { PORT } = process.env.PORT;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -29,6 +29,9 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(PORT, () => {
+const  PORT  = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+  console.log(`Escutando na porta 3000`)
   console.log(`Escutando na porta ${PORT}`);
 });
