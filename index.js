ec571f4798cfed7d783636b24a71b35246e8a465
negres <nataliaknegres@gmail.com>
2021-10-03 12:44:16 -0300

    Refaz configurações

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Escutando na porta ${PORT}`);
+app.listen(port, () => {
+  console.log(`Escutando na porta ${port}`);
 });
