33a42b47eee36c5688e87e28bfb8456b328c3352
Maria Baeta <m.martinsdg@gmail.com>
2021-10-04 18:52:28 -0300

    feat: :bug: Tentando

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = (UPSIDEDOWN_MODE === 'true');
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
