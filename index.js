5d0636067e9bff0fea41e97ea853c5c4040d81c1
rcalvs <rafaelcalvette@gmail.com>
2021-10-04 15:18:29 -0300

    trying to resolve H10 Heroku error

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Escutando na porta 3000');
 });
