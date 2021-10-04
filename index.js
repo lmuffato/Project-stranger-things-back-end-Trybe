90d1f2145a67556ac7493c380499e2d540354659
Rodrigo Rech <rodrigorech92@gmail.com>
2021-10-04 17:40:45 -0300

    Fix: PORT at index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
-const { PORT } = process.env || 3000;
+const PORT = process.env.PORT || 3000;
 
 const upsideDown = () => {
   if (process.env.UPSIDEDOWN_MODE === 'true') return true;
