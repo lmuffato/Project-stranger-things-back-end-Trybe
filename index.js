56a19c33cced64a070b733f3fb7f35c79f6841c1
w-cypriano <profwellington17@gmail.com>
2021-10-04 17:39:07 -0300

    fix requisito6

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const hereIsTheUpsideDown = false;
-
+// alteração
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
