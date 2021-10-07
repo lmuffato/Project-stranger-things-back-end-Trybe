b565ea8ceac0fed7c09cea752267e3a619b94cdd
jlfagundes <jlfagundes@gmail.com>
2021-10-07 10:07:53 -0300

    feat: challenge 6 - var upsidedown mode true/false

--- services/StrangerThings.js
+++ services/StrangerThings.js
@@ -10,7 +10,7 @@ class StrangerThingsService {
   search({ page, size, ...params }, upsideDownMode) {
     const characters = this.repository.search(params, { page, size });
 
-    if (upsideDownMode) {
+    if (upsideDownMode === 'true') {
       return characters.map(({ name, origin, status }) => ({
         name: flipout(name),
         origin: flipout(origin),
