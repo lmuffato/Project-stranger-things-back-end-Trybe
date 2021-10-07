70a4642be4248a8a097f01cbc54006d0b2ad2dff
Patrick Morais <ppternunes@gmail.com>
2021-10-07 17:58:05 -0300

    teste

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
+// aa
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
