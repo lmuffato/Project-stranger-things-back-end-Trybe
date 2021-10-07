d081ce4e469f612da3e475afad27585d815c5c4d
gabrieldeori <deori.pessoal@gmail.com>
2021-10-07 19:23:13 -0300

    Bugou

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+// mds buga mt
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
